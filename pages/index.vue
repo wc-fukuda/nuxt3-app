0cf742461e1aa5a2bd3e3d2af676cd82b86563b2
mai.fukuda <mai.fukuda@webcrew.co.jp>
2024-06-24 18:29:01 +0900

    Sec.15 相互ページ遷移できた

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
         <h1>top page</h1>
+        <hr>
+        <NuxtLink to="/price">Go to price page</NuxtLink>
     </div>
 </template>
--- pages/price.vue
+++ pages/price.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
         <h1>price page</h1>
+        <hr>
+        <NuxtLink to="/">Go to top page</NuxtLink>
     </div>
 </template>
