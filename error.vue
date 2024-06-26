3554ce0b4668bdf7cc78bcb5906f51dc5a2d44b4
mai.fukuda <mai.fukuda@webcrew.co.jp>
2024-06-26 12:07:52 +0900

    Sec.27 エラーページの作成

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div v-if="error.statusCode === 404">
+        <h1>Error</h1>
+        <p>{{ error.statusCode }}</p>
+        <p>{{ error.statusMessage }}</p>
+        <p>{{ error.message }}</p>
+    </div>
+</template>
+
+<script setup>
+    const error = useError()
+</script>
