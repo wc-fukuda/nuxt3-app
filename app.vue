0c47c801b1824e1e48ed3344cb0321aa237d4a5d
mai.fukuda <mai.fukuda@webcrew.co.jp>
2024-06-26 13:29:03 +0900

    Sec.39 画像の表示

--- app.vue
+++ app.vue
@@ -1,19 +1,5 @@
 <template>
   <div>
-    <ul>
-      <li v-for="user in users" :key="user.id">
-        {{ user.id }},{{ user.name }}
-      </li>
-    </ul>
+    <img src="~/assets/test.jpg" alt="SAMPLE">
   </div>
 </template>
-
-<script setup>
-  const { data: users, error } = await useFetch('https://jsonplaceholder.typicode.com/usersssssss')
-  // console.log(error)
-  if(error.value) {
-    throw createError({
-      statusCode: 404,
-      statusMessage: 'Not Found'})
-  }
-</script>
