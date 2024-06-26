9b5fa48633b1efc725bd25f2ce0180617a20ed50
mai.fukuda <mai.fukuda@webcrew.co.jp>
2024-06-26 12:25:27 +0900

    Sec.29 htmlのヘッダー情報設定（titleとwebフォントの設定）

--- app.vue
+++ app.vue
@@ -6,3 +6,17 @@
   </div>
 </template>
 
+<script setup>
+useHead({
+  title: 'Nuxt3 Store',
+  link: [
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap' }
+  ]
+})
+</script>
+
+<style>
+html {
+  font-family: 'Permanent Marker', cursive;
+}
+</style>
