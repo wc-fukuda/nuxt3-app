e2a05fe6ab01e178f43bd8c8073d0c2db599f349
mai.fukuda <mai.fukuda@webcrew.co.jp>
2024-07-08 13:03:11 +0900

    Sec.56-57 タスクの追加、削除、全件削除

--- app.vue
+++ app.vue
@@ -1,16 +1,45 @@
 <template>
   <h1>TODO</h1>
-  <input type="text">
-      <button>Add</button>
+  <form @submit.prevent="addTask">
+    <input v-model="newTask" name="addTask" autocomplete="off" type="text">
+    <button>Add</button>
+  </form>
   <ul>
-    <li>
-      <span>てすと</span>
-      <button>Delete</button>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="$event => deleteTask(index)">Delete</button>
     </li>
   </ul>
-  <button>All Clear</button>
+  <button @click="clearTask">All Clear</button>
 </template>
 
 <script setup>
+  // cookieに保持するための記述
+  const tasks = useCookie(
+    // 第一引数 key
+    'tasks',
+    // 第二引数 options
+    {
+      default: () => []
+    }
+  )
 
+  // 新しいタスクを追加するための記述
+  const newTask = ref('')
+  function addTask() {
+    if(newTask.value.length >= 1){
+      tasks.value.push(newTask.value)
+    }
+    newTask.value = ''
+  }
+
+  // タスクを削除するための記述
+  function deleteTask(index) {
+    tasks.value.splice(index, 1)
+  }
+
+  // 全てのタスクを削除するための記述
+  function clearTask() {
+    tasks.value = []
+  }
 </script>
