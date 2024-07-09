839c653e4affa8b89992dcc44292416a111308b7
mai.fukuda <mai.fukuda@webcrew.co.jp>
2024-07-09 17:11:42 +0900

    Sec.20 イベント処理の基礎（v-on:clickを使用）

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,7 @@
 <template>
     <div>
-      <ul>
-        <!-- valueでオブジェクトの値を取得 -->
-        <li v-for="value in user" :key="value">{{ value }}</li>
-      </ul>
-      <ul>
-        <!-- keyでオブジェクトのキーも取得できる -->
-        <!-- 第一引数にvalue, 第二引数にkeyを指定することで、キーと値を取得できる -->
-        <li v-for="(value, key) in user" :key="value">{{ key }}: {{ value }}</li>
-      </ul>
-      <ul>
-        <!-- 任意の変数名を指定することも可能 -->
-        <li v-for="(v, k) in user" :key="v">{{ v }}: {{ k }}</li>
-      </ul>
+        <button v-on:click="onclick">クリック</button>
+        <p>{{ now }}</p>
     </div>
 </template>
 
@@ -20,13 +9,19 @@
 export default {
     data() {
         return {
-          user: {
-            // key: value の形でデータを記述
-            firstName: 'John',
-            lastName: 'Doe',
-            age: 25
-          }
+          // 初期値を設定する
+          now: 'ここに現在時刻が表示されます'
         }
+    },
+    methods: {
+      onclick: function() {
+        // console.log('クリックされました')
+
+        // 現在時刻を取得してnowに代入する
+        this.now = new Date().toLocaleString()
+        // thisはnowを持っているオブジェクトを指す
+        console.log(this)
+      }
     }
 }
 </script>
