3df909cac3098201f90229af2fccf197ba25b1b9
mai.fukuda <mai.fukuda@webcrew.co.jp>
2024-07-09 16:25:32 +0900

    Sec17 v-ifで要素の表示非表示切り替え

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,6 @@
 <template>
-    <div id="app">
-        <!--  dataプロパティの値を表示 -->
-        <h1>{{ message }}</h1>
-        <p>{{ count }}</p>
-        <!--  userオブジェクトの値を表示 -->
-        <p>{{ user.name }}</p>
-        <p>{{ user.age }}</p>
-        <p>{{ user.prefecture }}</p>
-        <!--  colors配列の値を表示 -->
-        <p>{{ colors[0] }}</p>
-        <!--  colors配列の値を繰り返し表示 -->
-        <ul>
-            <li v-for="color in colors" :key="color">{{ color }}</li>
-        </ul>
+    <div>
+        <p v-if="toggle">hello</p>
     </div>
 </template>
 
@@ -20,14 +8,8 @@
 export default {
     data() {
         return {
-            message: 'Hello, Vue3!',
-            count: 99,
-            user: {
-                name: 'John Doe',
-                age: 30,
-                prefecture: 'Tokyo'
-            },
-            colors: ['red', 'blue', 'green']
+          // toggleの初期値をfalseに設定、trueにすると表示される
+          toggle: false
         }
     }
 }
