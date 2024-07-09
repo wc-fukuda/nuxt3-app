22e2450da902d26eb390bdf74cef2fdd7848c48a
mai.fukuda <mai.fukuda@webcrew.co.jp>
2024-07-09 16:54:29 +0900

    Sec.19 v-forでオブジェクトを繰り返し描画

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-      <ol>
-        <!-- 単数形 in 複数形とすることで配列だと認識しやすくなる -->
-        <!-- key="color" は、v-for でループする要素に一意のキーを指定しておく必要がある -->
-        <li v-for="color in colors" :key="color">{{ color }}</li>
-      </ol>
+      <ul>
+        <!-- valueでオブジェクトの値を取得 -->
+        <li v-for="value in user" :key="value">{{ value }}</li>
+      </ul>
+      <ul>
+        <!-- keyでオブジェクトのキーも取得できる -->
+        <!-- 第一引数にvalue, 第二引数にkeyを指定することで、キーと値を取得できる -->
+        <li v-for="(value, key) in user" :key="value">{{ key }}: {{ value }}</li>
+      </ul>
+      <ul>
+        <!-- 任意の変数名を指定することも可能 -->
+        <li v-for="(v, k) in user" :key="v">{{ v }}: {{ k }}</li>
+      </ul>
     </div>
 </template>
 
@@ -12,7 +20,12 @@
 export default {
     data() {
         return {
-          colors: ['red', 'blue', 'green', 'yellow', 'purple']
+          user: {
+            // key: value の形でデータを記述
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 25
+          }
         }
     }
 }
