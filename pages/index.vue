1232984269fa58c567aff04744c76a9e190645c2
mai.fukuda <mai.fukuda@webcrew.co.jp>
2024-08-19 16:10:33 +0900

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,5 @@
 <template>
-    <div>
-        <p><input v-model="message" type="text"></p>
-        <p><input v-model="message" type="text"></p>
-        <!-- $dataと書くと、dataオブジェクトのreturnされた値が取得できる -->
-        <pre>{{ $data }}</pre>
-    </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            message: 'Hello, Vue.js!'
-        }
-    }
-}
 </script>
